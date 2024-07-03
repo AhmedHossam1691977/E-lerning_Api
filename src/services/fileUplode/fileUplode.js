06ea6e6992f90dd49e81cd54efd74d7dc773c0af
AhmedHossam1691977 <ahmedhossam1691977@gmail.comg>
2024-07-04 02:36:55 +0300

    online dbwwgdfb

--- src/services/fileUplode/fileUplode.js
+++ src/services/fileUplode/fileUplode.js
@@ -1,40 +1,29 @@
-import mongoose from "mongoose";
-import multer from "multer";
-import {v4 as uuidv4} from "uuid" 
-import { AppError } from "../../utils/appError.js";
-
-
-
-export const fileUplode =()=>{
-
+import 'dotenv/config';
+export const fileUpload = () => {
     const storage = multer.diskStorage({
-        destination:  (req, file, cb)=> {
-            cb(null, 'uploads/')
+        destination: (req, file, cb) => {
+            // استخدم متغير البيئة UPLOADS_PATH أو استخدم "uploads/" كمسار افتراضي
+            const uploadsPath = process.env.UPLOADS_PATH || 'uploads/';
+            cb(null, uploadsPath);
         },
-        filename:  (req, file, cb)=> {
-            cb(null, new mongoose.Types.ObjectId + "-" + file.originalname)
+        filename: (req, file, cb) => {
+            cb(null, new mongoose.Types.ObjectId() + "-" + file.originalname);
+        },
+    });
+
+    function fileFilter(req, file, cb) {
+        if (file.mimetype.startsWith('image') || file.mimetype.startsWith('video/')) {
+            cb(null, true);
+        } else {
+            cb(new AppError('Only images and videos are allowed!', 401), false);
         }
-        })
+    }
 
+    const upload = multer({ storage, fileFilter });
 
-        function fileFilter(req, file, cb){
-            if(file.mimetype.startsWith('image')){
-                cb(null , true)
-            }else{
-                cb(new AppError('images only ',401) , false)
-            }
-    
-        }
+    return upload;
+};
 
-        const upload = multer({ storage ,fileFilter})
-
-        return upload
-}
-
-export const uploadSingleFile = fieldName => fileUplode().single(fieldName)
-export const uploadArrayOfFiles = fieldName=>  fileUplode().array(fieldName,10)
-export const uploadFields =fields=>  fileUplode().fields(fields)
-
-
-
-
+export const uploadSingleFile = (fieldName) => fileUpload().single(fieldName);
+export const uploadArrayOfFiles = (fieldName) => fileUpload().array(fieldName, 10);
+export const uploadFields = (fields) => fileUpload().fields(fields);
