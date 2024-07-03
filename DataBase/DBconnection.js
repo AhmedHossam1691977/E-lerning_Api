d6fb7ffb5d3a19c271440bda146b3931bcf5903f
AhmedHossam1691977 <ahmedhossam1691977@gmail.comg>
2024-07-03 19:13:30 +0300

    online db

--- DataBase/DBconnection.js
+++ DataBase/DBconnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 function connectionDB(){ 
     mongoose.set('strictQuery',true)
-    mongoose.connect("mongodb://127.0.0.1:27017/lesson").then(()=>{
+    mongoose.connect("mongodb+srv://Admin:AhmedHossam1691977@cluster0.8zhjqva.mongodb.net/lesson").then(()=>{
         // mongodb+srv://Admin:AhmedHossam1691977@cluster0.8zhjqva.mongodb.net/E-commerce
     console.log("datatbase conniction....");
 }).catch((err)=>{
