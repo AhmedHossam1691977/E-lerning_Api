ac9d57504ee2491a0caecd78f89bf91742eadcaf
Ahmed Hossam <ahmedhossam1691977@gmail.com>
2025-05-14 02:31:46 +0300

    update database

--- DataBase/DBconnection.js
+++ DataBase/DBconnection.js
@@ -3,7 +3,7 @@
 
     function connectionDB(){ 
         mongoose.set('strictQuery',true)
-        mongoose.connect("mongodb+srv://Admin:AhmedHossam1691977@cluster0.8zhjqva.mongodb.net/driver").then(()=>{
+        mongoose.connect("mongodb+srv://Admin:AhmedHossam1691977@cluster0.8zhjqva.mongodb.net/lesson").then(()=>{
             // mongodb+srv://Admin:AhmedHossam1691977@cluster0.8zhjqva.mongodb.net/E-commerce
         console.log("datatbase conniction....");
     }).catch((err)=>{
